f522312ccfa80de88ba61ecd326837acb4b51f16
twoh <hherdinaufal@gmail.com>
2016-08-14 22:53:07 +0700

    firsto commito

---
+++ src/main/java/id/web/twoh/easyqr/MainActivity.java
@@ -0,0 +1,90 @@
+package id.web.twoh.easyqr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import info.vividcode.android.zxing.CaptureActivity;
+import info.vividcode.android.zxing.CaptureActivityIntents;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvScanResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button btScan = (Button) findViewById(R.id.bt_scan);
+        btScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent captureIntent = new Intent(MainActivity.this, CaptureActivity.class);
+
+                CaptureActivityIntents.setPromptMessage(captureIntent, "Barcode scanning...");
+
+                startActivityForResult(captureIntent, 0);
+            }
+        });
+
+        tvScanResult = (TextView) findViewById(R.id.tv_scanresult);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 0) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                String value = data.getStringExtra("SCAN_RESULT");
+                tvScanResult.setText(value);
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                tvScanResult.setText("Scanning Gagal, mohon coba lagi.");
+            }
+        } else {
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
